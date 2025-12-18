db40b4f4324ddfd09e0f2dbb3d9f13b98a99a145
Juanfran Pagán <1913955@alu.murciaeduca.es>
2025-12-18 20:08:51 +0100

    vistas login, register y tabLoginRegister

---
+++ movil/app/src/main/java/onyx/movil/fragments/TabLoginRegisterFragment.kt
@@ -0,0 +1,41 @@
+package onyx.movil.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.tabs.TabLayoutMediator
+import onyx.movil.databinding.FragmentTabLoginRegisterBinding
+import onyx.movil.viewpager.TabLoginRegisterViewPagerAdapter
+
+class TabLoginRegisterFragment : Fragment() {
+    private lateinit var binding: FragmentTabLoginRegisterBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentTabLoginRegisterBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val adapter = TabLoginRegisterViewPagerAdapter(this)
+        binding.viewPager.adapter = adapter
+
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) {
+            tab, position -> tab.text = when (position) {
+                0 -> "Iniciar sesión"
+                1 -> "Registrarse"
+                else -> ""
+            }
+        }.attach()
+    }
+}
---
+++ movil/app/src/main/java/onyx/movil/viewpager/TabLoginRegisterViewPagerAdapter.kt
@@ -0,0 +1,19 @@
+package onyx.movil.viewpager
+
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import onyx.movil.fragments.LoginFragment
+import onyx.movil.fragments.RegisterFragment
+import onyx.movil.fragments.TabLoginRegisterFragment
+
+class TabLoginRegisterViewPagerAdapter(fragmentActivity: TabLoginRegisterFragment) : FragmentStateAdapter(fragmentActivity) {
+    override fun getItemCount(): Int = 2
+
+    override fun createFragment(position: Int): Fragment {
+        return when (position) {
+            0 -> LoginFragment()
+            1 -> RegisterFragment()
+            else -> LoginFragment()
+        }
+    }
+}
