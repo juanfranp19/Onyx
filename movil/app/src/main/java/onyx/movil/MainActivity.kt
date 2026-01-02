e08b02438f9dd92febbb5f284673f584993aad23
Juanfran Pagán <1913955@alu.murciaeduca.es>
2026-01-02 22:39:51 +0100

    estructura del scaffold

--- movil/app/src/main/java/onyx/movil/MainActivity.kt
+++ movil/app/src/main/java/onyx/movil/MainActivity.kt
@@ -2,10 +2,17 @@ package onyx.movil
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import onyx.movil.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /* TOOLBAR */
+        this.setSupportActionBar(binding.toolbar)
     }
 }
